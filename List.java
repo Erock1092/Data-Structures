e70a0b6495735acf721a20254d5e89379d098af6
Erock1092 <31455294+Erock1092@users.noreply.github.com>
2020-05-18 18:36:39 -0700

    Add files via upload

---
+++ List.java
@@ -0,0 +1,16 @@
+public interface List<E>{
+
+    public int size();
+    public boolean add(E e);
+    public void add(int index, E element);
+    public void clear();
+    public boolean contains(Object o);
+    public boolean equals(Object o);
+    public E get(int index);
+    public int hashCode();
+    public int indexOf(Object o);
+    public boolean isEmpty();
+    public E remove(int index);
+    public E remove(Object o);
+    
+}
