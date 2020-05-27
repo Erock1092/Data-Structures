4aad9c7ecacabd6a9d37359b2c57e595549d897e
ackle003 <50431311+ackle003@users.noreply.github.com>
2020-05-26 20:41:26 -0700

    added removal and traversal

--- _structs/BinarySearchTree.java
+++ _structs/BinarySearchTree.java
@@ -1,8 +1,7 @@
 package _structs;
 
-import java.util.Iterator;
 
-public class BinarySearchTree<T extends Comparable> implements Iterable<T> {
+public class BinarySearchTree<T extends Comparable>{
 
     protected class Node<T extends Comparable> {
         private Node left = null;
@@ -48,6 +47,7 @@ public class BinarySearchTree<T extends Comparable> implements Iterable<T> {
                 insert(item, node.right);
             }
         }
+        size++;
         return true;
     }
     protected boolean insert(Node item){
@@ -56,12 +56,7 @@ public class BinarySearchTree<T extends Comparable> implements Iterable<T> {
     public boolean insert(T data){
         return insert(new Node(data));
     }
-    
-    @Override
-    public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+   
    public void inorderTraversal(){
       inorder(root);
    }
@@ -93,4 +88,76 @@ public class BinarySearchTree<T extends Comparable> implements Iterable<T> {
            System.out.printf("%s\s\s", root.data);
         }
    }
+   public int size(){
+       return size;
+   }
+ 
+   protected T get(T item){
+       Node temp = root;
+       if(temp == root)
+        return (T)root.data;
+
+        while(!item.equals(temp.data)){
+
+            if(temp.data.compareTo(item) == 0){
+                return (T)temp.data;
+            }
+            else if(temp.data.compareTo(item) < 0){
+                temp = root.left;
+            }
+            else{
+                temp = root.right;
+            }
+        }
+        return null;
+   }
+   protected Node contains(Node node, T item){
+       int compare = item.compareTo(node.data);
+       if(compare == 0){
+           return node;
+        }
+        else if(compare < 0){
+            contains(node.left, item);
+        }
+        else{
+            contains(node.right, item);}
+        return null;
+        
+    }
+    public boolean contains(T item){
+        return !contains(root, item).equals(null);
+    }
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+    public boolean remove(T item){
+        return !remove(item, root).equals(null);
+    }
+    protected Node remove(T item, Node node){
+        if(contains(item)){
+            if(item.compareTo(node.data) == 0){
+                Node temp = node;
+                node.data = removeMinumum(node.right);
+                size--;
+                return temp;
+            }
+            else if(item.compareTo(node.data) < 0){
+                remove(item, node.left);
+            }
+            else if(item.compareTo(node.data) > 0){
+                remove(item, node.right);
+            }
+        }
+        return null;
+    }
+    protected T removeMinumum(Node node){
+        Node temp = node;
+        while(!temp.equals(null)){
+            temp = temp.left;
+
+    }
+        T data = (T) temp.data;
+        node = null;
+        return data;
+    }
 }
