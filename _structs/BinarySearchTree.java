f6979000270566fbf01f5783b575462a6d7c6c59
ackle003 <50431311+ackle003@users.noreply.github.com>
2020-05-28 14:17:39 -0700

    added delete method

--- _structs/BinarySearchTree.java
+++ _structs/BinarySearchTree.java
@@ -54,7 +54,7 @@ public class BinarySearchTree<T extends Comparable>{
         return insert(item, root);
     }
     public boolean insert(T data){
-        if(root.data == null){
+        if(root == null){
 
             root = new Node(data);
             return true;
@@ -129,5 +129,78 @@ public class BinarySearchTree<T extends Comparable>{
     public boolean isEmpty(){
         return size() == 0;
     }
-
+    protected T getMin(Node node){
+        T data = (T) node.data;
+        while(node.left != null){
+            data = (T) node.data;
+            node = node.left;
+        }
+        return data;
+    }
+    protected Node delete(T data, Node node){
+        if(node == null) return node;
+        
+        if(data.compareTo(node.data) > 0){
+                
+            delete(data, node.right);
+            
+        }
+            
+        else if(data.compareTo(node.data) < 0){
+                
+            delete(data, node.left);
+            
+        }
+        else{
+            if(node.left == null){
+                return node.right;
+            }
+            else if(node.right == null){
+                return node.left;
+            }
+            node.data = getMin(node.right);
+            node.right = delete((T) node.data, node.right);
+        }
+        return node;
+    }
+    
+    public void delete(T data){
+        delete(data, root);
+    }
+    public static void main(String[] args) 
+    { 
+        BinarySearchTree tree = new BinarySearchTree(); 
+  
+        /* Let us create following BST 
+              50 
+           /     \ 
+          30      70 
+         /  \    /  \ 
+        20   40  60   80 */
+        tree.insert(50); 
+        tree.insert(30); 
+        tree.insert(20); 
+        tree.insert(40); 
+        tree.insert(70); 
+        tree.insert(60); 
+        tree.insert(80); 
+  
+        System.out.println("Inorder traversal of the given tree"); 
+        tree.inorderTraversal(); 
+  
+        System.out.println("\nDelete 20"); 
+        tree.delete(20); 
+        System.out.println("Inorder traversal of the modified tree"); 
+        tree.inorderTraversal(); 
+  
+        System.out.println("\nDelete 30"); 
+        tree.delete(30); 
+        System.out.println("Inorder traversal of the modified tree"); 
+        tree.inorderTraversal(); 
+  
+        System.out.println("\nDelete 50"); 
+        tree.delete(50); 
+        System.out.println("Inorder traversal of the modified tree"); 
+        tree.inorderTraversal(); 
+    } 
 }
