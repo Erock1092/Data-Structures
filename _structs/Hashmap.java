340fda29e3a90ebe4a9b065fab17a5dc97e61c22
ackle003 <50431311+ackle003@users.noreply.github.com>
2020-05-29 11:41:30 -0700

    Hashmap with chaining

---
+++ _structs/Hashmap.java
@@ -0,0 +1,85 @@
+package _structs;
+
+
+import java.util.LinkedList;
+
+public class Hashmap<K,V> {
+    private final static int SIZE = 10;
+    private final static int NOT_FOUND = -1;
+    private class HashNode{
+        private K key;
+        private V value;
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+        private V getValue(K key){
+            if(key.equals(this.key)){
+                return value;
+            }
+            return null;
+        }
+        private boolean containsKey(K key){
+            return this.key == key;
+        }
+        private void setValue(V value){
+            this.value = value;
+        }
+    }
+    private LinkedList<HashNode>[] hashmap;
+    public Hashmap(){
+        for(int i = 0; i < SIZE; i++){
+            hashmap[i] = new LinkedList<HashNode>();
+        }
+    }
+    private int computeHash(K key){
+        return key.hashCode();
+    }
+    public void put(K key, V value){
+        int hash = computeHash(key);
+        HashNode node = new HashNode(key, value);
+
+        if(!hashmap[hash].contains(node)){
+            int indexOfKey = containsKeyAt(key, hashmap[hash]);
+            if(indexOfKey == NOT_FOUND){
+
+                hashmap[hash].add(node);
+                return;
+            }
+            else{
+                
+                hashmap[hash].get(indexOfKey).setValue(value); 
+            }
+        }
+    }
+    public V get(K key){
+        int hash = computeHash(key);
+        for(HashNode node: hashmap[hash]){
+            V temp = node.getValue(key);
+            if(temp != null){
+                return temp;
+            }
+        }
+        return null;
+    }
+    private int containsKeyAt(K key, LinkedList<HashNode> lis){
+            HashNode temp =  lis.element();
+           if(temp.containsKey(key)){
+               return 0;
+           }
+           else{
+               for(int i = 1; i < lis.size(); i++){
+                   if(lis.get(i).containsKey(key)) {
+                       return i;
+                   }
+               }
+           }
+           return NOT_FOUND;
+    }
+    public void clear(){
+        for(LinkedList<HashNode> lis: hashmap){
+            lis.clear();
+        }
+    }
+    
+}
