8cb2d7677aabce735eea8a8498d9c150a26474d7
ackle003 <50431311+ackle003@users.noreply.github.com>
2020-05-30 15:38:02 -0700

    Generic Graph

---
+++ _structs/Graph.java
@@ -0,0 +1,70 @@
+package _structs;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+
+public class Graph<T> {
+    private Map<T, LinkedList<T>> adjacencies; 
+    
+    public void addVertex(T vertex){
+        if(adjacencies.containsKey(vertex)){
+            throw new IllegalArgumentException();
+        }
+        adjacencies.put(vertex, new LinkedList<T>());
+    }
+    public Graph(){
+        adjacencies = new HashMap<T, LinkedList<T>>();
+    }
+    public Graph(java.util.Collection<T> vertices){
+        adjacencies = new HashMap<T, LinkedList<T>>(vertices.size());
+        for(T data: vertices){
+            adjacencies.put(data, new LinkedList<T>());
+        }
+    }
+    public void connect(T from, T to){
+
+        if(from == to){ throw new IllegalArgumentException(); }
+
+        if(adjacencies.containsKey(from) && adjacencies.containsKey(to)){
+            adjacencies.get(from).add(to);
+            adjacencies.get(to).add(from);
+        }
+        
+    }
+    public boolean contains(T data){
+        return adjacencies.containsKey(data);
+    }
+    public boolean remove(T data){
+        LinkedList<T> temp = adjacencies.get(data);
+        for(T vertice: temp){
+            LinkedList<T> adj = adjacencies.get(vertice);
+            adj.remove((Object) data);
+        }
+        return adjacencies.remove(data, temp);
+    }
+    public String toString(){
+        StringBuilder string = new StringBuilder();
+        for(T vertex: adjacencies.keySet()){
+
+            string.append(vertex.toString() + ": ");
+
+            for(T connections: adjacencies.get(vertex)){
+
+                string.append(connections.toString() + " ");
+            }
+            string.append("\n");
+        }
+        return string.toString();
+    }
+    public static void main(String[] args) {
+        Graph <Integer> g = new Graph<Integer>();
+        g.addVertex(5);
+        g.addVertex(7);
+        g.connect(5, 7);
+        g.addVertex(6);
+        g.connect(6, 7);
+        System.out.printf(g.toString());
+    }
+}
